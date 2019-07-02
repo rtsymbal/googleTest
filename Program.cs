aff924e689eef8e832f5d63d422f2813f4376fc9
Roman Tsymbal <roman.tsymbal@gmail.com>
2019-07-02 20:40:56 +0200

    creation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
-namespace googleTest
+namespace TestASPNETCore
 {
     class Program
     {
-        static void Main(string[] args)
+        IWebDriver driver = new ChromeDriver("D:\\Development");
+
+        [SetUp]
+        public void Initialize()
         {
-            Console.WriteLine("Hello World!");
+            driver.Navigate().GoToUrl("http://www.google.pl");
+            Console.WriteLine("Get to Google page");
         }
+
+        [Test]
+        public void TestExecution()
+        {
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("First Test");
+            Console.WriteLine("Enter test in search field");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Close the browser");
+        }
+
     }
 }

90fc13714dd225385f2fd6f56e29adc0b02980be
Roman Tsymbal <roman.tsymbal@gmail.com>
2019-07-13 22:49:13 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,31 +1,40 @@
-﻿using googleTest;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
 
-namespace TestASPNETCore
+namespace googleTest
 {
+    [TestFixture]
     class Program
     {
 
         [SetUp]
         public void Initialize()
         {
-            PropertiesCollection.Driver = new ChromeDriver("D:\\Development");
+            PropertiesCollection.Driver = new ChromeDriver();
 
             PropertiesCollection.Driver.Navigate().GoToUrl("http://www.executeautomation.com/demosite/index.html?UserName=5712654&Password=5546464&Login=Login");
-            Console.WriteLine("Get to page");
+            Console.WriteLine("Get to the page");
         }
 
         [Test]
         public void TestExecution()
         {
+
+            MainPage page = new MainPage();
+
+            page.SelectDD("Mr.");
+            page.InputTxt("Roman");
+            page.ClickSave();
+
+
             //Title selection
-            SeleniumSetMethods.SelectDropDown("TitleId", "Mr.", PropertyType.Id);
+            //SeleniumSetMethods.SelectDropDown("TitleId", "Mr.", PropertyType.Id);
 
             //Initial filling
-            SeleniumSetMethods.EnterText("Initial", "Roman", PropertyType.Name);
+            //SeleniumSetMethods.EnterText("Initial", "Roman", PropertyType.Name);
 
             //Check Title value
             Console.WriteLine("The value of Title is: " + SeleniumGetMethods.GetTextFromDDL("TitleId", PropertyType.Id));
@@ -34,8 +43,8 @@ namespace TestASPNETCore
             Console.WriteLine("The value of Initial is: " + SeleniumGetMethods.GetText("Initial", PropertyType.Id));
 
             //Click Save button
-            SeleniumSetMethods.ClickSmth("Save", PropertyType.Name);
-            
+            //SeleniumSetMethods.ClickSmth("Save", PropertyType.Name);
+
         }
 
         [TearDown]
